d0360afba3d3a0301a5b54d14f10e3dc860a3ce2
suilahi <suilahizakaria@gmail.com>
2025-04-23 17:52:29 +0100

    post list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostFormComponent } from './post-form/post-form.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PostListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
