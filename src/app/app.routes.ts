faeac1c3d6cccc60fdbd1348d0eb457a2228ff46
ridaDev <kiratterida@gmail.com>
2025-04-25 12:17:39 +0100

    modification de code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { AproposComponent } from './apropos/apropos.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { ContactComponent } from './contact/contact.component';
+import { PostListComponent } from './post-list/post-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : '',
+        component : HomeComponent
+    },
+    
+    {
+        path : 'login',
+        component : LoginComponent
+    },
+    {
+        path : 'register',
+        component : RegisterComponent
+    },
+    {
+        path : 'apropos',
+        component : AproposComponent
+
+    },
+    {
+        path : 'post',
+        component : PostDetailComponent
+    },
+    {
+        path : 'cantact',
+        component : ContactComponent
+    },
+    {
+        path : 'categorie',
+        component : PostListComponent
+    }
+];
